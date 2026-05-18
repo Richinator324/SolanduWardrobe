ce576ae1fa8f5d988957fc82f8a6a433eaf01c54
Richinator324 <Richinator324@gmail.com>
2026-05-18 13:55:23 -0400

    Potential final commit?

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ const overlayClothing = (clothingPath) => {
 
 // Function to update the 3D viewer with selected clothing
 const updateSkin = async () => {
-    const clothingPath = `./${document.getElementById("clothingSelect").value}.png`;
+    const clothingPath = `/${document.getElementById("clothingSelect").value}.png`;
 
     try {
         // Apply the selected clothing overlay
